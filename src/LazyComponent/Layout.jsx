16fecefe9c287e4b247de460983bcbf565199749
qiaozhididi <1343013603@qq.com>
2024-12-01 20:42:38 +0800

    Suspense组件的使用

--- src/LazyComponent/Layout.jsx
+++ src/LazyComponent/Layout.jsx
@@ -1,13 +1,33 @@
-import React, { Component } from "react";
+import React, { Component, Suspense } from "react";
+import ErrorBoundary from "./ErrorBoundary";
 // import LazyComponent from "./LazyComponent";
 const LazyComponent = React.lazy(() => import("./LazyComponent"));
 
 export default class Layout extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      flag: false,
+    };
+  }
+  clickHandler = () => {
+    this.setState({
+      flag: !this.state.flag,
+    });
+  };
+
   render() {
     return (
       <div>
         <h3>Layout</h3>
-        <LazyComponent />
+        <button onClick={this.clickHandler}>点击</button>
+        {this.state.flag && (
+          <ErrorBoundary>
+            <Suspense fallback={<div>等待...</div>}>
+              <LazyComponent />
+            </Suspense>
+          </ErrorBoundary>
+        )}
       </div>
     );
   }
